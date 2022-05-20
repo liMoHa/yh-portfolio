897a6254e59a74923bdda8c9d9d836d51ea9cc10
liMoHa <dudgk6382@gmail.com>
2022-05-20 11:54:49 +0900

    fix: change directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import MenuBar from "./components/MenuBar/Index";
+import Home from "./components/Home/Index";
 import About from "./components/About/Index";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import MenuBar from "./components/MenuBar";
-import Project from "./components/Project/Index";
 import Skills from "./components/Skills/Index";
+import Project from "./components/Project/Index";
+import Contact from "./components/Contact/Index";
 import { GlobalStyle } from "./styles/global-style";
 
 const Container = styled.div`
